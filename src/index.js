5ba0d0045883a332466ba6393f5e93b59d2bc81f
salihsimseek@gmail.com <salihsimseek@gmail.com>
2023-01-18 17:06:56 +0300

    base configuration

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//GraphQL Client Configuration
+import { ApolloProvider } from '@apollo/client'
+import client from './apollo'
+
+//Router
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
